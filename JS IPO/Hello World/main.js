3717671ea0912ab60e04572ce6d4cb8735379e76
RealOGKing <a.oguntade11@gmail.com>
2023-02-07 13:07:48 -0700

    Pizza Order inicializacion

--- JS IPO/Hello World/main.js
+++ JS IPO/Hello World/main.js
@@ -1,1 +1,6 @@
-let username;
+let username = prompt("What is your name?");
+let timeOfDay = prompt("What is your time of day?");
+
+let msg = "Hello " + username + "! have a great " + timeOfDay + ".";
+
+alert(msg);
