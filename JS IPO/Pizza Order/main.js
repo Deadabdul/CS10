d612b2311ea53073af7e82b8668d977052bf42de
RealOGKing <a.oguntade11@gmail.com>
2023-02-07 22:02:41 -0700

    Pizza Order v2

--- JS IPO/Pizza Order/main.js
+++ JS IPO/Pizza Order/main.js
@@ -5,14 +5,14 @@ document.getElementById("btn").addEventListener("click", getOrder);
 
 function getOrder() {
     // INPUT
-    let size = prompt("Enter the size of pizza (small, medium, large): ");
-    let topping1 = prompt("Enter first topping: ");
-    let topping2 = prompt("Enter second topping: ");
+    let size = document.getElementById("size").value;
+    let topping1 = document.getElementById("topping1").value;
+    let topping2 = document.getElementById("topping2").value;
 
     //PROCESS
     let msg = "your " + size + " pizza with " + topping1 + " and " + topping2 +
     " will be ready soon!";
     
     //OUTPUT
-    alert(msg);
+    document.getElementById("output").innerHTML = (msg);
 }
