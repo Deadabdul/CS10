860b094bd1de3adbfdf202681f9a25f2ca4dea3a
RealOGKing <a.oguntade11@gmail.com>
2023-02-08 12:41:17 -0700

    Mad Lib Completion

--- JS IPO/Mad Lib/main.js
+++ JS IPO/Mad Lib/main.js
@@ -1,5 +1,22 @@
-document.getElementById("disguise").addEventListener("click", () =>{alert("disguise")});
-document.getElementById("dozen").addEventListener("click", () =>{alert("dozen")});
-document.getElementById("bush").addEventListener("click",   () =>{alert("bush")});
-document.getElementById("never").addEventListener("click",   () =>{alert("never")});
-document.getElementById("leg").addEventListener("click",   () =>{alert("leg")});
+
+document.getElementById("btn").addEventListener("click", CreateMadlib);
+
+
+
+function CreateMadlib() {
+    // INPUT
+    let place = document.getElementById("place").value;
+    let name1 = document.getElementById("name1").value;
+    let name2 = document.getElementById("name2").value;
+    let verb = document.getElementById("verb").value;
+    let noun = document.getElementById("noun").value;
+
+    //PROCESS
+    let msg = `While in ${place}, ${name1} and ${name2} had a ${verb}
+    compotition, the looser had to had to do what ever the winnder
+    chose, when ${name1} lost, ${name2}  made him eat a ${noun}.`;
+
+    
+    //OUTPUT
+    document.getElementById("output").innerHTML = (msg);
+}
