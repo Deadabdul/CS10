c9ccd323dfaf3531bf763a140378fd259972dead
RealOGKing <a.oguntade11@gmail.com>
2023-02-15 12:34:53 -0700

    lul

---
+++ JS IPO/Simple Addition Calculator/main.js
@@ -0,0 +1,8 @@
+
+
+function Calculate(){
+    let number1 = document.getElementById("number1").value;
+    let number2 = document.getElementById("number2").value;
+
+    document.getElementById("result").innerHTML = number1 + " + " + number2;
+}
