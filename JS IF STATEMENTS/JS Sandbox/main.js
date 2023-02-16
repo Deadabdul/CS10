acf96c8ba29fce40c3fc23e37776e8cada74d440
RealOGKing <a.oguntade11@gmail.com>
2023-02-16 13:28:09 -0700

    aD

--- JS IF STATEMENTS/JS Sandbox/main.js
+++ JS IF STATEMENTS/JS Sandbox/main.js
@@ -3,9 +3,9 @@ document.getElementById("btn").addEventListener("click", btnClicked)
 
 function btnClicked() {
     //input
-    let input = document.getElementById("input").value;
+    
     //process
-    let average = input;
+    let average;
 
     //output
     output.innerHTML = average;
